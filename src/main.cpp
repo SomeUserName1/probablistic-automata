04c1a576b3cd640fe42929c6c22d818222edfe61
Fabian Klopfer <fabian.klopfer@ieee.org>
2020-05-14 22:03:33 +0200

    adjusted example

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,58 @@
-#include <differential_equations/DifferentialEquationModel.h>
-#include <weighted_automata/WeightedAutomatonModel.h>
+#include "differential_equations/DifferentialEquationModel.h"
+#include "weighted_automata/WeightedAutomatonModel.h"
 #include "ui/TextUserInterface.cpp"
 
 int main(int argc, char* argv[]) {
-    auto* ui = new TextUserInterface();
-    auto task = ui->select_task();
+    UserInterface::Task task;
+    ModelInterface model;
+    ReductionMethodInterface reductionMethod;
+    UserInterface::IOMethod inputMethod;
+    UserInterface::IOMethod outputMethod;
+    std::string outputDestination;
+    std::string input;
+
+    if (argc == 0) {
+        auto ui = std::make_unique<TextUserInterface>(TextUserInterface());
+        task = ui->select_task();
+
+        switch (task) {
+            case UserInterface::Reduction: {
+                std::vector<ModelInterface> models = {WeightedAutomatonModel(), DifferentialEquationModel()};
+                model = ui->select_model(models);
+                reductionMethod = ui->select_reduction_method(model);
+                inputMethod = ui->select_io_method();
+                outputMethod = ui->select_io_method();
+
+                if (inputMethod == UserInterface::IOMethod::File) {
+                    input = ui->file_input();
+                } else {
+                    input = ui->stdin_input();
+                }
+                if (outputMethod == UserInterface::IOMethod::File) {
+                    outputDestination = ui->set_output_destination();
+                }
+                break;
+            }
+            case UserInterface::Benchmark:
+            case UserInterface::Conversion:
+            case UserInterface::Unselected:
+                throw NotImplementedException();
+        }
+    } else if (argc == 1) {
+    } else {
+
+    }
 
     switch (task) {
         case UserInterface::Reduction: {
-            std::vector<ModelInterface> models = {WeightedAutomatonModel(), DifferentialEquationModel()};
-            const auto& model = ui->select_model(models);
-            const auto& reductionMethod = ui->select_reduction_method(model);
-            const auto inputMethod = ui->select_io_method();
-            std::string input;
-            if (inputMethod == TextUserInterface::IOMethod::File) {
-                input = ui->file_input();
-            } else {
-                input = ui->stdin_input();
-            }
             const auto representation = model.validate_model_instance(input);
             const auto reduced_representation = reductionMethod.reduce(representation);
-            const auto outputMethod = ui->select_io_method();
             const auto summary = model.summarize_reduction(representation, reduced_representation);
-            if (outputMethod == TextUserInterface::IOMethod::File) {
-                const auto& outputDestination = ui->set_output_destination();
-                ui->display_file(summary, outputDestination);
+
+            if (outputMethod == UserInterface::IOMethod::File) {
+                TextUserInterface::display_file(summary, outputDestination);
+            } else if (outputMethod == UserInterface::IOMethod::Stdin) {
+                TextUserInterface::display_stdout(summary);
             }
             break;
         }
@@ -33,6 +61,7 @@ int main(int argc, char* argv[]) {
         case UserInterface::Unselected:
             throw NotImplementedException();
     }
+
 }
 
 
